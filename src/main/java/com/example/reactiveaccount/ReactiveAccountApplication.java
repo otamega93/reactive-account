1b8a6fe8010a29132e27443222bb2251d3912034
Manuel Páez <m.paez589@gmail.com>
2018-08-13 10:17:08 +0200

    Add files via upload

---
+++ src/main/java/com/example/reactiveaccount/ReactiveAccountApplication.java
@@ -0,0 +1,12 @@
+package com.example.reactiveaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveAccountApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/reactiveaccount/ReactiveAccountApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.reactiveaccount;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveAccountApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
